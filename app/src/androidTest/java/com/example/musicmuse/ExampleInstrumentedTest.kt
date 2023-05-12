8794e84ee5f47e1668c478078064c9681885dda7
Shashank <pshashank569@gmail.com>
2023-05-12 21:33:26 +0530

    renamed app

--- app/src/androidTest/java/com/example/musicmuse/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/musicmuse/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.musicmuse", appContext.packageName)
+        assertEquals("com.musicmuse.app", appContext.packageName)
     }
 }
