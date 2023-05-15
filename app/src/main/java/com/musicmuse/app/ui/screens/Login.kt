2034b5ab1851fb3629641cb908f5a350b10ad479
Shashank <pshashank569@gmail.com>
2023-05-15 09:27:21 +0530

    login elements (wip)

--- app/src/main/java/com/musicmuse/app/ui/screens/Login.kt
+++ app/src/main/java/com/musicmuse/app/ui/screens/Login.kt
@@ -1,9 +1,56 @@
+@file:Suppress("FunctionName")
+
 package com.musicmuse.app.ui.screens
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Login() {
-    Text("Sup brah lets login")
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+
+    Scaffold { padding ->
+        Box(Modifier.padding(padding).fillMaxHeight()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(Modifier.padding(0.dp)) {
+                    Column {
+                        TextField(
+                            value = email,
+                            leadingIcon = { Icon(Icons.Rounded.Email, "Email") },
+                            placeholder = { Text("Email") },
+                            onValueChange = { email = it },
+                            colors = TextFieldDefaults.textFieldColors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            )
+                        )
+                        TextField(
+                            value = password,
+                            leadingIcon = { Icon(Icons.Rounded.Delete, "Password") },
+                            placeholder = { Text("Password") },
+                            onValueChange = { password = it },
+                            colors = TextFieldDefaults.textFieldColors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
